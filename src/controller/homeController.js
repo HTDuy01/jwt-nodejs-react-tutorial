31e87d89c29cdd6a3896ab0ecfedaade038ef2d1
HTDuy01 <htd0901@gmail.com>
2023-09-07 15:11:25 +0700

    create new user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,37 @@
-const handleHelloWorld = (req, res) => {
-  return res.render("home.ejs")
-} 
+// get the client
+import mysql from "mysql2";
 
-const handleUserPage = (req, res) => { 
-  return res.render("user.ejs")
-}
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "jwt",
+});
+
+const handleHelloWorld = (req, res) => {
+  return res.render("home.ejs");
+};
+
+const handleUserPage = (req, res) => {
+  return res.render("user.ejs");
+};
+
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+
+  // simple query
+  connection.query("INSERT INTO users (email, password, username) VALUES(?, ?, ?)", [email, password, username], function (err, results, fields) {
+    if (err) {
+      console.error(err);
+    }
+  });
+  return res.send("application");
+};
 
 module.exports = {
-  handleHelloWorld,handleUserPage
-}
+  handleHelloWorld,
+  handleUserPage,
+  handleCreateNewUser,
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,18 +1,19 @@
 import express from "express";
-import homeController from '../controller/homeController'
+import homeController from "../controller/homeController";
 
 const router = express.Router();
 
 /**
- * 
+ *
  * @param {*} app : express app
  */
 const initWebRouter = (app) => {
   //path,handler
-  router.get("/", homeController.handleHelloWorld)
-  router.get('/user',homeController.handleUserPage)
+  router.get("/", homeController.handleHelloWorld);
+  router.get("/user", homeController.handleUserPage);
+  router.post("/users/create-user", homeController.handleCreateNewUser);
 
-  return app.use('/',router)
-}
+  return app.use("/", router);
+};
 
 export default initWebRouter;
