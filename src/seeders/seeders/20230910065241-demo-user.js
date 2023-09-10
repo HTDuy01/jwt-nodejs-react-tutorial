4fcc52b9540c9ab60c2cba53b3bd17e363830cad
HTDuy01 <htd0901@gmail.com>
2023-09-10 14:01:30 +0700

    gererate model/migration/seeder

---
+++ src/seeders/seeders/20230910065241-demo-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+    // Fake các user cho databases
+      {
+      email: 'John Doe',
+      password: '123',
+      username:'fake1'
+    },
+    {
+      email: 'John Doe',
+      password: '123',
+      username:'fake2'
+    },
+    {
+      email: 'John Doe',
+      password: '123',
+      username:'fake3'
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
